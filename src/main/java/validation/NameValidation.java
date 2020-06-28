e40b49d6060f2fb152056d08581d391135da582d
Asike <sapasask@gmail.com>
2020-06-28 21:22:28 +0600

    issues corrected

--- src/main/java/validation/NameValidation.java
+++ src/main/java/validation/NameValidation.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 
 public class NameValidation {
     public static final String regexEng = "^[aA-zZ]\\w{3,29}$";
-    public static final String regexRu = "^[А-Я]{1}[а-я]{1,29}$";
-    public  boolean isValidUserName(String name) {
+    public static final String regexRu = "^[А-Я]{1,29}[а-я]{1,29}$";
 
+    public  boolean isValidUserName(String name) {
 
         Pattern patternEng = Pattern.compile(regexEng);
         Pattern patternRu = Pattern.compile(regexRu);
